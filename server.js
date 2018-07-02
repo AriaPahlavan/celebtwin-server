3b906e0d7bed5696f8b2813ae78c37104dec5a38
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-02 18:57:47 -0500

    update the singin POST API to hash user passbords using bscrypt

--- server.js
+++ server.js
@@ -70,13 +70,25 @@ app.get('/profile/:id', (req, res) => {
 
 
 app.post('/signin', (req, res) => {
-  const body = req.body;
+  const {email, password} = req.body;
 
-  databaseContains(
-    user => body.email === user.email && body.password === user.password,
-    user => res.json(user),
-    () => res.status(400).json('error logging in')
-  );
+
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        db.select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('unable to get user');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 });
 
 app.post('/register', (req, res) => {
